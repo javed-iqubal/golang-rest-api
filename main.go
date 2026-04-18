1cb6912c478eee41ab6cd055aeb0e282508197e4
javed-iqubal <jiqbal07@gmail.com>
2026-04-18 20:12:40 +0530

    added find put and delete features

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"sync"
 
 	"github.com/go-chi/chi/v5"
@@ -26,9 +27,15 @@ func main() {
 	r := chi.NewRouter()
 
 	// route
-	r.Route("/book", func(r chi.Router) {
+	r.Route("/books", func(r chi.Router) {
 		r.Get("/", GetAllBooks)
 		r.Post("/", CreateBook)
+
+		r.Route("/{id}", func(r chi.Router) {
+			r.Get("/", GetBook)       // Get books/1
+			r.Put("/", UpdateBook)    // Put books/1
+			r.Delete("/", DeleteBook) // Delete books/1
+		})
 
 	})
 	fmt.Println("Server running on http://localhost:8080")
@@ -69,4 +76,65 @@ func CreateBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(book)
+}
+
+// Get books/1
+func GetBook(w http.ResponseWriter, r *http.Request) {
+
+	idStr := chi.URLParam(r, "id")
+	id, _ := strconv.Atoi(idStr)
+
+	mu.RLock()
+	book, exists := books[id]
+	mu.RUnlock()
+
+	if !exists {
+		http.Error(w, "Book not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "Application/json")
+	json.NewEncoder(w).Encode(book)
+
+}
+
+// PUT books/1
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+
+	idStr := chi.URLParam(r, "id")
+	id, _ := strconv.Atoi(idStr)
+
+	var updated Book
+
+	if err := json.NewDecoder(r.Body).Decode(&updated); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	mu.RLock()
+	if _, exists := books[id]; !exists {
+		mu.RUnlock()
+		http.Error(w, "Book not found", http.StatusNotFound)
+		return
+	}
+
+	updated.ID = id
+	books[id] = updated
+	mu.RUnlock()
+
+	w.Header().Set("Content-Type", "Application/json")
+	json.NewEncoder(w).Encode(updated)
+}
+
+// Delete books/1
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+
+	idStr := chi.URLParam(r, "id")
+	id, _ := strconv.Atoi(idStr)
+
+	mu.RLock()
+	delete(books, id)
+	mu.RUnlock()
+
+	w.WriteHeader(http.StatusNoContent)
 }
